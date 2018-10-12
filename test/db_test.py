fa8e9621246547ca9dc5055869b24d1e8b9dd676
AikoYanye <aikoyanye@gmail.com>
2018-10-13 02:17:54 +0800

    测试pymysql

---
+++ test/db_test.py
@@ -0,0 +1,61 @@
+import pymysql
+
+HOST = 'localhost'
+USERNAME = 'aiko'
+PASSWORD = 'aiko1234'
+DATABASE = 'final_design'
+
+# 打开数据库连接
+db = pymysql.connect(HOST, USERNAME, PASSWORD, DATABASE)
+cursor = db.cursor()
+
+# 插入操作
+# sql = """
+#     INSERT INTO member (username, phone, reputation, created) VALUES
+#     ('{}', '{}', '{}', '{}')
+#     """.format('雾雨魔理沙', '13239245134', '良', '2018-10-13 01:50:XX')
+# try:
+#     cursor.execute(sql)
+#     db.commit()
+# except:
+#     db.rollback()
+
+# 查询操作
+# fetchone() 获取单条
+# fetchall() 多条
+# sql = """
+#     SELECT * FROM member
+#     """
+# sql1 = """
+#     SELECT * FROM member WHERE username = {}
+#     """.format('"雾雨魔理沙"')
+# try:
+#     cursor.execute(sql)
+#     for result in cursor.fetchall():
+#         # 每个result是一个元组
+#         print(result)
+# except:
+#     pass
+
+# 更新数据
+# sql = """
+#     UPDATE member SET phone = {}, username = {}
+#     WHERE id = 2
+#     """.format('"11"', '"雾雨魔理沙"')
+# try:
+#     cursor.execute(sql)
+#     db.commit()
+# except:
+#     db.rollback()
+
+# 删除数据
+# sql = """
+#     DELETE FROM member WHERE id = 3
+#     """
+# try:
+#     cursor.execute(sql)
+#     db.commit()
+# except:
+#     db.rollback()
+
+db.close()
