ab3c146d2d87eb8e0e06f6805eb7fb3c7fd55425
AikoYanye <aikoyanye@gmail.com>
2018-10-28 00:10:39 +0800

    181027

--- tool/some_tool.py
+++ tool/some_tool.py
@@ -1,7 +1,19 @@
-import datetime
+import datetime, requests
+
+WEATHER_API = 'https://api.seniverse.com/v3/weather/now.json?key=wj3xlpuivdrhutm1&location=ip&language=zh-Hans'
 
 class SomeTool:
     # 获取当前时间
     @staticmethod
     def current_date():
         return datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+    # 获取ip所在地的天气
+    @staticmethod
+    def weather():
+        resp = requests.get(WEATHER_API).json()
+        result = resp['results'][0]
+        return result['location']['name'] + '：' + result['now']['temperature'] + '摄氏度，' + result['now']['text']
+
+
+print(SomeTool.weather())
